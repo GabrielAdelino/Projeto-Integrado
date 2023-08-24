6df76f979690e161da905ab46be72564214ee8b5
GabrielAdelino <gabriel.adelinodev@gmail.com>
2023-08-24 16:19:57 -0300

    Adição carousel

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-var menuItem = document.querySelectorAll('.item-menu')
 
-function selectLink(){
- menuItem.forEach((item)=>
-  item.classList.remove('ativo')
- )
- this.classList.add('ativo')
-}
-
-menuItem.forEach((item)=>
- item.addEventListener('click', selectLink)
-)
 
 /*JS tela login/Cadastro */
 
